4104e8da2a73182e117210b824d7e20415f4b64c
Kevin Bennett <flightgod@gmail.com>
2025-04-25 15:06:19 -0500

    UI and Code Update
    Updated the UI to show description, and added firmware paths to S3 Bucket

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 const badgeSelect = document.getElementById('badgeSelect');
 const badgeImage = document.getElementById('badgeImage');
 const flashButton = document.getElementById('flashButton');
+const badgeDescriptions = {
+  basicQACode25: "For QA of the base baord",
+  cactuscon2025: "Official badge for CactusCon 2025, featuring ESP32-s3",
+  bsideskc25: "BSidesKC 2025 badge: Available after the event"
+};
 
 badgeSelect.addEventListener('change', () => {
   const selected = badgeSelect.value;
@@ -9,10 +14,14 @@ badgeSelect.addEventListener('change', () => {
     badgeImage.src = `/firmware/${selected}/badge.jpg`;
     badgeImage.style.display = 'block';
 
+    badgeDescription.innerText = badgeDescriptions[selected] || "";
+    badgeDescription.style.display = 'block';
+
     flashButton.manifest = `/firmware/${selected}/manifest.json`;
     flashButton.style.display = 'inline-block';
   } else {
     badgeImage.style.display = 'none';
+    badgeDescription.style.display = 'none';
     flashButton.style.display = 'none';
   }
 });
