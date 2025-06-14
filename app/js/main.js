ee01ec2ce92678131a1ff75ea3ab4830af635b97
Patrick Ecord <pecord@gmail.com>
2025-06-13 20:57:17 -0500

    fix bsideskc manifest

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,19 @@ const badgeDescriptions = {
   cactuscon2025: "Official badge for CactusCon 2025, featuring ESP32-s3",
   bsideskc25: "BSidesKC 2025 badge: Available after the event"
 };
-// Map of badge to manifest URLs (pointing to S3)
+// Map of badge to manifest URLs (switch between S3 and localhost for local testing)
+const useLocalhost = window.location.hostname === "localhost";
+const localPort = 8080;
 const manifestUrls = {
-  basicQACode25: "https://badgepirates-firmware.s3.amazonaws.com/basicQACode25/manifest.json",
-  cactuscon2025: "https://badgepirates-firmware.s3.amazonaws.com/cactuscon2025/manifest.json",
-  bsideskc25: "https://badgepirates-firmware.s3.amazonaws.com/bsideskc25/manifest.json"
+  basicQACode25: useLocalhost
+    ? `http://localhost:${localPort}/firmware/basicQACode25/manifest.json`
+    : "https://badgepirates-firmware.s3.amazonaws.com/basicQACode25/manifest.json",
+  cactuscon2025: useLocalhost
+    ? `http://localhost:${localPort}/firmware/cactuscon2025/manifest.json`
+    : "https://badgepirates-firmware.s3.amazonaws.com/cactuscon2025/manifest.json",
+  bsideskc25: useLocalhost
+    ? `http://localhost:${localPort}/firmware/bsideskc25/manifest.json`
+    : "https://badgepirates-firmware.s3.amazonaws.com/bsideskc25/manifest.json"
 };
 
 const badgeImages = {
